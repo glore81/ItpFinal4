ee1967f887d5c70c943d6505db1f36b6e250bdd4
Aldiyar <foove71@gmail.com>
2026-05-16 14:20:40 +0500

    Task funcitons to_dict, from_dict added

--- models/task.py
+++ models/task.py
@@ -10,3 +10,34 @@ class Task:
     def __str__(self):
         return (str("Task Id: " + str(self.task_id) + "\nTitle: "+self.title)+"\nDescription: "+self.description +
                 "\nPriority: " + self.priority + "\nDeadline: " + self.deadline + "\nStatus: " + self.status);
+
+    def mark_completed(self):
+        self.status = "completed";
+
+    def update(self, title = None, description = None, priority = None, deadline = None, status = None):
+
+        if title != None:
+            self.title = title;
+
+        if description != None:
+            self.description = description;
+
+        if priority != None:
+            self.priority = priority;
+
+        if deadline != None:
+            self.deadline = deadline;
+
+        if status != None:
+            self.status = status;
+
+    def to_dict(self):
+        return {"id" : self.task_id,
+                "title" : self.title,
+                "description" : self.description,
+                "priority" : self.priority,
+                "deadline" : self.deadline,
+                "status" : self.status}
+
+def from_dict(data):
+    return Task(data["id"], data["title"], data["description"], data["priority"],data["deadline"], data["status"])
