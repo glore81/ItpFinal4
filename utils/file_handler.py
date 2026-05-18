761b0911f4e9808665ef86f11a93cc5bf5288685
Aluana <Muslimova>
2026-05-18 23:21:11 +0500

    2 step errors safety

--- utils/file_handler.py
+++ utils/file_handler.py
@@ -3,13 +3,23 @@ from models.task import from_dict
 
 
 def task_to_json(filename,task_manager):
-    with open(filename,'r') as f:
-        tasks = json.load(f)
-    for task in tasks:
-        task = from_dict(task)
-        task_manager.add_task(task)
-        print("Loaded!")
-    return True
+    try:
+        with open(filename,'r') as f:
+            tasks = json.load(f)
+        for task in tasks:
+            task = from_dict(task)
+            task_manager.add_task(task)
+            print("Loaded!")
+        return True
+    except FileNotFoundError:
+        print("File not found!")
+        return False
+    except Exception as e:
+        print(e)
+        return False
+
+
+
 
 
 
