2c82c9eaa60cfa9028d21ee1069d6873af54f5e1
Aluana <Muslimova>
2026-05-19 08:42:52 +0500

    commit 6? just fixing some things

--- tests/test_task_manager.py
+++ tests/test_task_manager.py
@@ -3,18 +3,18 @@ from services.task_manager import TaskManager
 from models.task import Task
 
 class TaskManagerTest(unittest.TestCase):
-    def creat(self):
+    def setUp(self):
         self.manager = TaskManager()
         self.task = Task(1,"Test", "Description", "low", "2026-11-08", "pending")
         self.manager.add_task(self.task)
     def test_add_task(self):
-        new_task=Task(2, "fresh", "Description", "medium", "2026-11-08", "in progress")
+        new_task=Task(2, "fresh", "Description", "medium", "2026-11-08", "in_progress")
         self.manager.add_task(new_task)
         self.assertEqual(len(self.manager.tasks), 2)
     def test_delete_task(self):
         end=self.manager.delete_task(1)
         self.assertTrue(end)
-        self.assertEqual(len(self.manager.tasks), 1)
+        self.assertEqual(len(self.manager.tasks), 0)
     def test_mark_task(self):
         mark=self.manager.mark_completed(1)
         self.assertTrue(mark)
