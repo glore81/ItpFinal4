6f1b7464c9e75858056fd208a4d696ccaa8ffcda
Aluana <Muslimova>
2026-05-19 06:30:46 +0500

    creating unittest 2 commit, trying to add tasks

--- tests/test_task_manager.py
+++ tests/test_task_manager.py
@@ -5,5 +5,12 @@ from models.task import Task
 class TaskManagerTest(unittest.TestCase):
     def creat(self):
         self.manager = TaskManager()
-        self.task = Task(1,"Test", "Description", "low", "2026-11-08", "parsing")
+        self.task = Task(1,"Test", "Description", "low", "2026-11-08", "pending")
         self.manager.add_task(self.task)
+    def test_add_task(self):
+        new_task=Task(2, "fresh", "Description", "medium", "2026-11-08", "in progress")
+        self.manager.add_task(new_task)
+        if len(self.manager.tasks)==2:
+            return True
+        else:
+            return False
